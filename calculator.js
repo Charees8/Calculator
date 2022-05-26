4adc62a05052212687b5f4f35fec600afb5fd316
Charees8 <tlgbadebo@gmail.com>
2022-05-26 05:36:49 -0700

    first commit

---
+++ calculator.js
@@ -0,0 +1,26 @@
+let result = document.querySelector("#inputext");
+
+// let calculate = (number)=>{
+// result.value += number;
+// };
+
+function calculate(number) {
+    result.value += number;
+}
+
+let Result=()=>{
+    try{
+        result.value = eval(result.value)
+    }
+    catch(err){
+        alert("Enter the valid result");
+    }
+}
+
+function clr(){
+    result.value= "";
+}
+
+function del(){
+    result.value=result.value.slice(0,-1);
+}
